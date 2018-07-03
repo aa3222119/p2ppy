971216c5cfebd72da87c72ff7a43d312281369fa
antony <xuechen.han@weicheche.cn>
2018-07-03 16:53:19 +0800

    0.3.1

--- _peer_config.py
+++ _peer_config.py
@@ -9,5 +9,9 @@
 # DT表示发起code的时间间隔(s),
 # suit_peers : 向imforma询问可用peer，小于10s表示不主动发起,
 # peer_mining : 挖矿的时间间隔, 大于30的取值表示不挖矿
+# peer_mining_delay 第一次开始挖矿的延迟
 DT_second = {'suit_peers': 59,
-             'peer_mining': 2}
+             'peer_mining': 3,
+             'peer_mining_delay': 0}
+
+Bool_test_ground = True
