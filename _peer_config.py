4e27053250d7666da2d172b5cb07c0b8a4cdd9c4
antony <xuechen.han@weicheche.cn>
2018-07-03 17:39:41 +0800

    0.3.3

--- _peer_config.py
+++ _peer_config.py
@@ -7,11 +7,12 @@
 # ===========================================
 
 # DT表示发起code的时间间隔(s),
-# suit_peers : 向imforma询问可用peer，小于10s表示不主动发起,
-# peer_mining : 挖矿的时间间隔, 大于30的取值表示不挖矿
+# suit_peers :
+# peer_mining :
 # peer_mining_delay 第一次开始挖矿的延迟
-DT_second = {'suit_peers': 59,
-             'peer_mining': 3,
-             'peer_mining_delay': 0}
+DT_second = {'suit_peers': 59,  # 向imforma询问可用peer，小于10s表示不主动发起
+             'peer_mining': 31,  # 挖矿的时间间隔, 大于30的取值表示不挖矿
+             'peer_mining_delay': 1800  # 第一次开始挖矿的延迟
+             }
 
-Bool_test_ground = True
+Bool_test_ground = False
